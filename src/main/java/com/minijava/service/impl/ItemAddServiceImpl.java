690f6bf9a3cf672d598aca7e198c5d3c846540f9
gusgh075 <gusgh075@gmail.com>
2025-12-06 19:49:38 +0900

    chore : ItemAddServiceImpl의 생성자와 DI 접근지정자 재설정. itemAdd에 이름 중복 아이템 등록 처리 로직 추가.

--- src/main/java/com/minijava/service/impl/ItemAddServiceImpl.java
+++ src/main/java/com/minijava/service/impl/ItemAddServiceImpl.java
@@ -5,16 +5,28 @@ import com.minijava.repository.ItemRepository;
 import com.minijava.repository.ItemRepositoryInterface;
 import com.minijava.service.ItemAddService;
 
-public class ItemAddServiceImpl implements ItemAddService {
-    final ItemRepositoryInterface itemRepository;
+import java.util.List;
 
-    ItemAddServiceImpl(ItemRepository itemRepository) {
+public class ItemAddServiceImpl implements ItemAddService {
+    private final ItemRepositoryInterface itemRepository;
+
+    public ItemAddServiceImpl(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
     }
 
 
     @Override
     public int itemAdd(String name, String rank, String quality, int power) {
+        // 이름 중복 아이템 처리
+        List<ItemDTO> byName = itemRepository.findByName(name);
+        if(byName.isEmpty()){
+            System.out.println("이름이 중복됩니다. 아이템을 다시 등록해주세요.");
+            for (ItemDTO itemDTO : byName) {
+                System.out.println("중복되는 아이템 정보 => "+itemDTO);
+            }
+            return 0;
+        }
+        // 아이템 정상 등록
         int newItemNumber = itemRepository.getLastItemNumber() + 1;
         itemRepository.save(new ItemDTO(newItemNumber, name, rank, quality, power));
         return newItemNumber;
